fdf84d4e8ff4c6d726bb3b52883cedd6f20ec9d1
KiruiTito <titokirui402@gmail.com>
2023-03-30 15:33:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// returnFirstTwoDrivers function implementation
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // returnLastTwoDrivers function implementation
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // selectingDrivers array containing two functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier higher-order function implementation
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  // fareDoubler function implementation using createFareMultiplier
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler function implementation using createFareMultiplier
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers function implementation
+  const selectDifferentDrivers = function(drivers, selectFn) {
+    return selectFn(drivers);
+  };
+  
